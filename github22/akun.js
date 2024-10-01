8930cb6ea9f0b529aac8406727d66b6eca9ae2eb
anugrahhhhh <anugrahputa270@gmail.com>
2024-10-01 19:34:35 +0700

    Akun Informatika BP22

---
+++ github22/akun.js
@@ -0,0 +1,25 @@
+const users = [
+    { name: "Anugrah Putra Al Fatih", username: "anugrahhhhh" },
+    { name: "Galant Anefsyah Pratama", username: "GalantAP" },
+    { name: "Jovanka Sabila Pertiwi", username: "jovankasabilap" },
+    { name: "Putri Kamila", username: "Putrikamila30" },
+    { name: "Muhammad Fatieh Akram Faritshi", username: "fatihfaritshi" },
+    { name: "Najwa Alawiyah Siregar", username: "najwasiregar" },
+    { name: "Sepfliana Qethi", username: "Sepfliana" },
+    { name: "Rudi Kurnia Al Amin", username: "Ucokgagah" },
+    { name: "Jimmy Mahendra", username: "Jimmymahendra174" },
+    { name: "Dzaki Khoirullah Winadri", username: "DzakiKhoi" },
+    { name: "Gies Septiara", username: "giesseptiara" },
+    { name: "Syaira Amani Fatihah", username: "Syaira1323" },
+    { name: "Inka Taruni Sastra", username: "inkataruni" },
+    { name: "Mauliza Aprilia", username: "MaulizaAprilia" },
+    { name: "Kimi Maulana", username: "OrewaKimi" },
+    { name: "Indra Yones", username: "indrayones" },
+    { name: "Ade Adinda", username: "adeadinda" }
+  ];
+  
+  // Function to display the list of users
+  users.forEach(user => {
+    console.log(`${user.name} (${user.username})`);
+  });
+  
